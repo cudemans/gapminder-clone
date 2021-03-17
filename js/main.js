9c47946076e1b26461820e27b9ecdffc73192b15
Chris Udemans <chris@udemans.xyz>
2021-03-17 09:05:33 +0200

    corrected tooltip

--- js/main.js
+++ js/main.js
@@ -19,10 +19,10 @@ const g = svg.append("g")
 const tip = d3.tip()
 	.attr("class", "d3-tip")
 	.html(d => {
-		let text = `<strong>Country:</strong> <span style="color: red"; text-transform: capitalize>${d.country}</span><br>`
+		let text = `<strong>Country:</strong> <span style="color: red; text-transform: capitalize">${d.country}</span><br>`
 		text += `<strong>Continent:</strong> <span style="color: red; text-transform: capitalize">${d.continent}</span><br>`
 		text += `<strong>Life Expectancy:</strong> <span style="color: red">${d3.format(".2f")(d.life_exp)}</span><br>`
-		text += `<strong>GDP Per Capita:</strong> <span style="color: red">${d3.format("$,0f")(d.income)}</span><br>`
+		text += `<strong>GDP Per Capita:</strong> <span style="color: red">${d3.format("$,.0f")(d.income)}</span><br>`
 		text += `<strong>Population:</strong> <span style="color: red">${d3.format(",.0f")(d.population)}</span><br>`
 		return text
 	})
